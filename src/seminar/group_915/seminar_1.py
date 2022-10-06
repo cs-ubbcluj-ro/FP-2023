bcd1d7f00d185d214b1a4fceff397fbec768918b
Arthur Molnar <71508710+test-student-fp@users.noreply.github.com>
2022-10-06 12:53:36 +0300

    Example code for the first seminar.

--- src/seminar/group_915/seminar_1.py
+++ src/seminar/group_915/seminar_1.py
@@ -8,47 +8,21 @@ Problem statements to choose from:
 """
 
 """
-1. Given 2 ints, a and b, return True if one of them is 10 or if their sum is 10
-Question – What happens if we enter a non-integer number, or alphanumeric characters?
+Create a function in Python that accepts a single word and returns the number of vowels in that word. In this function, 
+only a, e, i, o, and u will be counted as vowels — not y.
 """
 
-"""
-2. Write a Python program which iterates the integers from 1 to 50. 
-For multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
-For numbers which are multiples of both three and five print "FizzBuzz".
-"""
 
-"""
-3. Calculate the first n terms of the Fibonacci sequence
-"""
+def count_vowles(word):
+    count = 0
+    vowels = "aeiou"
 
-"""
-4. Write a Python program to calculate body mass index.
-   How do we validate the code above for user input?
-   Let's write the specification for it
-"""
+    for char in word:
+        if char in vowels:
+            count += 1
 
-"""
-4. Given a non-empty string like "Code" return a string like "CCoCodCode"
-    string_splosion('Code') → 'CCoCodCode'
-    string_splosion('abc') → 'aababc'
-    string_splosion('ab') → 'aab'
-"""
+    return count
 
-"""
-5. Given 2 strings, a and b, return the number of the positions where they contain the same length 2
-substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the
-same place in both strings.
-    stringMatch('xxcaazz', 'xxbaaz') → 3
-    stringMatch('abc','abc) → 2
-        stringMatch('abc', 'axc') → 0
-"""
 
-"""
-6. Write a Python program to remove all duplicate elements from a given array and returns a new array.
-"""
-
-"""
-7. Return the sum of the numbers in a list, returning 0 for an empty list. Except the number 13 is very
-unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
-"""
+oh_my_word = input("Word to count vowels for: ")
+print("There are " + str(count_vowles(oh_my_word)) + " in word " + oh_my_word)
