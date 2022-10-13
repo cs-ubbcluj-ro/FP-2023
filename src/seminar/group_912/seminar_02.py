a9117e62722f83d6356efbae609cb5308a64b66f
Arthur Molnar <arthur486@gmail.com>
2022-10-13 10:52:01 +0300

    try/except and types example

--- src/seminar/group_912/seminar_02.py
+++ src/seminar/group_912/seminar_02.py
@@ -4,3 +4,29 @@ Let's create a menu-driven application. This is the menu:
     2. Sort the list of numbers
     0. Exit
 """
+
+
+def start():
+    while True:
+        print("1. Generate rational numbers")
+        print("2. Sort the list of numbers")
+        print("0. Exit")
+
+        opt = -1
+        try:
+            opt = int(input(">"))  # by default reads str
+        except ValueError as ve:
+            print(ve)
+
+        # print(type(opt))
+        if opt == 1:
+            pass
+        elif opt == 2:
+            pass
+        elif opt == 0:
+            return  # or break
+        else:
+            print("Bad command or file name :)")
+
+
+start()
