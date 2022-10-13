4f7a973f4e1be358de700556533fdcf08ad7e45f
Arthur Molnar <arthur486@gmail.com>
2022-10-13 11:17:59 +0300

    seminar work.

--- src/seminar/group_912/seminar_02.py
+++ src/seminar/group_912/seminar_02.py
@@ -1,4 +1,5 @@
 from traceback import print_exc
+import random
 
 """
 Let's create a menu-driven application. This is the menu:
@@ -6,6 +7,25 @@ Let's create a menu-driven application. This is the menu:
     2. Sort the list of numbers
     0. Exit
 """
+
+"""
+    Functions for Q numbers
+"""
+
+
+def create_q(num, denom=1):
+    # TODO What about 1/0 !!
+    return [num, denom]
+
+
+def generate_q():
+    """
+    1. Ask user how many numbers
+    2. Generate random num (in -10,10), denom (1, 20)
+    3. Print the numbers
+    3. Return the list of numbers
+    """
+    pass
 
 
 def start():
