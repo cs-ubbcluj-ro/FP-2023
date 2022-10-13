848095a085e7ec0628ecf580d94c90c13d727fa0
Arthur Molnar <arthur486@gmail.com>
2022-10-13 12:27:57 +0300

    First version of the main menu!

--- src/seminar/group_915/seminar_02.py
+++ src/seminar/group_915/seminar_02.py
@@ -4,3 +4,25 @@ Let's write a menu-driven application to work with points in 2D space.
     2. Sort the points by distance from center
     0. Exit
 """
+
+
+def start():
+    while True:
+        print("1. Generate points in 2D space")
+        print("2. Sort the points by distance from center")
+        print("0. Exit")
+
+        opt = input(">")
+        # print(type(opt))
+
+        if opt == "1":
+            pass
+        elif opt == "2":
+            pass
+        elif opt == "0":
+            return  # or break
+        else:
+            print("Bad command (or file name) :)")
+
+
+start()
