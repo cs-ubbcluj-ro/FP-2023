83df1e069d8a0905ed1ecb190fb3390629cc6bb6
Arthur Molnar <arthur486@gmail.com>
2022-11-03 10:18:57 +0200

    Problem statements for group 912

---
+++ src/seminar/group_912/seminar_05.py
@@ -0,0 +1,28 @@
+"""
+    Dynamic programming
+"""
+
+"""
+1. Calculate the maximum subarray sum (subarray = elements having continuous indices)
+
+    e.g.
+    for data = [-2, -5, 6, -2, -3, 1, 5, -6], maximum subarray sum is 7.
+"""
+
+"""
+2. Count in how many ways we can provide change to a given sum of money (N), when provided infinite
+   supply of given coin denominations.
+
+   e.g. Let's say N = 10, and we have coins of values (1, 5, 10); we can give change in 4 ways (10, 5 + 5, 5 + 1 + ... 
+   and 1 + ... + 1)
+"""
+
+"""
+3. 0-1 Knapsack problem. Given the weights and values of N items, put them in a knapsack having capacity W so that you
+   maximize the value of the stored items. Items cannot be broken up (0-1 property)
+"""
+
+"""
+4. Gold mine problem (a.k.a checkerboard problem)
+   https://www.geeksforgeeks.org/gold-mine-problem
+"""
