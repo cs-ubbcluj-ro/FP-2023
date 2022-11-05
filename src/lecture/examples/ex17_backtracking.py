06daa7c6fabfc966f130406339b9a27226c78959
Arthur Molnar <arthur486@gmail.com>
2022-11-06 00:23:30 +0200

    Added iterative backtracking example.

--- src/lecture/examples/ex17_backtracking.py
+++ src/lecture/examples/ex17_backtracking.py
@@ -20,24 +20,38 @@ def generate_test(array, dim):
         generate_test(array[:], dim)
 
 
-def backtracking(array, dim):
+def backtracking_iter(dim: int):
+    array = [-1]  # candidate solution
+    while len(array) > 0:
+        chosen = False
+        while not chosen and array[-1] < dim - 1:
+            array[-1] = array[-1] + 1  # increase the last component
+            chosen = len(set(array)) == len(array)
+        if chosen:
+            if len(array) == dim:
+                print(array)
+            array.append(-1)  # expand candidate solution
+        else:
+            array = array[:-1]  # go back one component
+
+
+def backtracking_rec(array, dim):
     if len(array) == dim:
         print(array)
-        pass
     if len(array) > dim:
         return
     array.append(0)
     for i in range(0, dim):
         array[-1] = i
         if len(set(array)) == len(array):
-            backtracking(array, dim)
+            backtracking_rec(array, dim)
             array.pop()
 
 
 '''
 And here we build our experiment
 '''
-functions = [generate_test, backtracking]
+functions = [generate_test, backtracking_rec]
 data_sizes = [3, 4, 5, 6, 7]
 
 t = Texttable()
