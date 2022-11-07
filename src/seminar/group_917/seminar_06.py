8bfab91a55207819f6e1d03781ab236eafef5e05
Arthur Molnar <arthur486@gmail.com>
2022-11-07 10:05:53 +0200

    Seminar 6 problem statement for group 917

---
+++ src/seminar/group_917/seminar_06.py
@@ -0,0 +1,71 @@
+"""
+Write an application that manages a group of students.
+Each student has a unique id (string), a name (string) and a grade (integer).
+The application will have a menu-driven user interface and will provide the
+following features:
+
+    1. Add a student
+        - adds the student with the given id, name and grade to the list.
+        - error if giving existing id, the name or grade fields not given or
+        empty
+
+    2. Delete a student
+        - deletes the student with the given id from the list
+        - error if non-existing id given
+
+    3. Show all students
+        - shows all students in descending order of their grade
+
+    4. Show students whose grade is > than given one, ordered in descending
+    order of grade
+
+    5. exit
+        - exit the program
+
+    Observations:
+        - Add 10 random students at program startup
+        - Write specifications for non-UI functions
+        - Each function does one thing only, and communicates via parameters and return value
+        - The program reports errors to the user. It must also report errors from non-UI functions!
+        - Make sure you understand the student's representation
+        - Try to reuse functions across functionalities (Less code to write and test)
+        - Don't use global variables!
+"""
+
+#
+# Write the implementation for Seminar 06 in this file
+#
+
+#
+# Write below this comment
+# Functions to deal with students -- list representation
+# -> There should be no print or input statements in this section
+# -> Each function should do one thing only
+# -> Functions communicate using input parameters and their return values
+#
+
+#
+# Write below this comment
+# Functions to deal with students -- dict representation
+# -> There should be no print or input statements in this section
+# -> Each function should do one thing only
+# -> Functions communicate using input parameters and their return values
+#
+
+#
+# Write below this comment
+# Functions that deal with the required functionalities properties
+# -> There should be no print or input statements in this section
+# -> Each function should do one thing only
+# -> Functions communicate using input parameters and their return values
+#
+
+#
+# Write below this comment
+# UI section
+# Write all functions that have input or print statements here
+# Ideally, this section should not contain any calculations relevant to program functionalities
+#
+
+if __name__ == "__main__":
+    print("Make magic happen")
