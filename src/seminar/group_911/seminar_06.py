3e2d6c61885c34654f1b8ded4a2fd28aae9c2e20
Arthur Molnar <arthur486@gmail.com>
2022-11-07 08:03:09 +0200

    Seminar 06 work for 911

---
+++ src/seminar/group_911/seminar_06.py
@@ -0,0 +1,71 @@
+"""
+Write an application that manages a list of circles.
+Each circle has a unique center (x,y - ints) and a positive radius (int).
+The application will have a menu-driven user interface and will provide the following features:
+
+    1. Add a circle
+        - adds the given circle to the list.
+        - error if circle with given center already exists, the center or radius not given, empty or radius < 0
+
+    2. Delete a circle
+        - deletes the circle with the given center
+        - error if non-existing center is given
+
+    3. Show all circles
+        - shows all circles in descending order of their radius
+
+    4. Show circles that intersect a given one
+        - select a circle from the list of existing circles
+        - print those which intersect it
+        (bonus: sort printed circles by descending order of radius)
+
+    5. exit
+        - exit the program
+
+    Observations:
+        - Add 10 random circles at program startup
+        - Write specifications for non-UI functions
+        - Each function does one thing only, and communicates via parameters and return value
+        - The program reports errors to the user. It must also report errors from non-UI functions!
+        - Make sure you understand the circle's representation
+        - Try to reuse functions across functionalities (Less code to write and test)
+        - Don't use global variables!
+"""
+
+#
+# Write the implementation for Seminar 06 in this file
+#
+
+#
+# Write below this comment
+# Functions to deal with circles -- list representation
+# -> There should be no print or input statements in this section
+# -> Each function should do one thing only
+# -> Functions communicate using input parameters and their return values
+#
+
+#
+# Write below this comment
+# Functions to deal with circles -- dict representation
+# -> There should be no print or input statements in this section
+# -> Each function should do one thing only
+# -> Functions communicate using input parameters and their return values
+#
+
+#
+# Write below this comment
+# Functions that deal with the required functionalities properties
+# -> There should be no print or input statements in this section
+# -> Each function should do one thing only
+# -> Functions communicate using input parameters and their return values
+#
+
+#
+# Write below this comment
+# UI section
+# Write all functions that have input or print statements here
+# Ideally, this section should not contain any calculations relevant to program functionalities
+#
+
+if __name__ == "__main__":
+    print("Make magic happen")
