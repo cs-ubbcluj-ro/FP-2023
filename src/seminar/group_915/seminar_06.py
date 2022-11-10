6fbb0daea6622cae4bcff0df42cce9ffc34c8d2b
Arthur Molnar <arthur486@gmail.com>
2022-11-10 10:07:40 +0200

    Starter code seminar 6

---
+++ src/seminar/group_915/seminar_06.py
@@ -0,0 +1,73 @@
+"""
+Write an application that manages a list of rectangles.
+Each rectangle is represented using its two opposite corners (x1, y1) and (x2, y2)
+The application will have a menu-driven user interface and will provide the following features:
+
+    1. Add a rectangle
+        - adds the given rectangle to the list.
+        - error if the given rectangle already exists, x1 <= x2 or y1 <= y2
+
+    2. Delete a rectangle
+        - deletes the rectangle with the given corners
+        - error if non-existing rectangle is given
+
+    3. Show all rectangles
+        - shows all rectangles in descending order of their area
+
+    4. Show rectangles that intersect a given one
+        - select a rectangle from the list of existing rectangle
+        - print those which intersect it by descending order of area
+
+    5. exit
+        - exit the program
+
+    Observations:
+        - Add 10 random rectangles at program startup
+        - Write specifications for non-UI functions
+        - Each function does one thing only, and communicates via parameters and return value
+        - The program reports errors to the user. It must also report errors from non-UI functions!
+        - Make sure you understand the rectangle's representation
+        - Try to reuse functions across functionalities (Less code to write and test)
+        - Don't use global variables!
+"""
+
+#
+# Write the implementation for Seminar 06 in this file
+#
+
+#
+# Write below this comment
+# Functions to deal with rectangles -- list representation
+# -> There should be no print or input statements in this section
+# -> Each function should do one thing only
+# -> Functions communicate using input parameters and their return values
+#
+
+
+#
+# Write below this comment
+# Functions to deal with rectangles -- dict representation
+# -> There should be no print or input statements in this section
+# -> Each function should do one thing only
+# -> Functions communicate using input parameters and their return values
+#
+
+
+#
+# Write below this comment
+# Functions that deal with the required functionalities properties
+# -> There should be no print or input statements in this section
+# -> Each function should do one thing only
+# -> Functions communicate using input parameters and their return values
+#
+
+
+#
+# Write below this comment
+# UI section
+# Write all functions that have input or print statements here
+# Ideally, this section should not contain any calculations relevant to program functionalities
+#
+
+if __name__ == "__main__":
+    print("Let's go!")
