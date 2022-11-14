dcadd7b2785853bbf18ff2834f81c270173d9681
Arthur Molnar <arthur486@gmail.com>
2022-11-14 08:19:40 +0200

    Seminar 7 structure

---
+++ src/seminar/group_911/seminar_07/__init__.py
@@ -0,0 +1,16 @@
+"""
+Tic Tac Toe board game
+    - Human vs. computer player
+    - Human plays first
+    - Computer always plays valid moves
+
+Command-driven UI !!!???
+    play 1,1  # (1,1) is center square on the 3x3 board
+    takeback  # takes back human player's last move
+    ragequit
+
+What to look out for today:
+    1. Modular programming
+    2. Test-Driven Development (first examples)
+    3. Using Exceptions
+"""
