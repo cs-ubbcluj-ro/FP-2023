428480a0323241cafc74869823d392173732efc3
Arthur Molnar <arthur486@gmail.com>
2022-11-28 08:42:37 +0200

    updated car class with property for id

--- src/seminar/group_911/seminar_09/domain/car.py
+++ src/seminar/group_911/seminar_09/domain/car.py
@@ -7,22 +7,39 @@ class car:
     """
 
     def __init__(self, car_id: str, make: str, model: str, color: str):
-        pass
+        self.__car_id = car_id
+        self.__make = make
+        self.__model = model
+        self.__color = color
+
+    @property
+    def car_id(self):
+        return self.__car_id
+
+    @car_id.setter
+    def car_id(self, new_value):
+        self.__car_id = new_value
 
 
 def test_car():
     new_car = car("CJ 01 ABC", "Dacia", "Sandero", "red")
-    assert new_car.get_id() == "CJ 01 ABC"
-    assert new_car.get_make() == "Dacia"
-    assert new_car.get_model() == "Sandero"
-    assert new_car.get_color() == "red"
+    # assert new_car.get_id() == "CJ 01 ABC"
+    assert new_car.car_id == "CJ 01 ABC"
+    assert new_car.make == "Dacia"
+    assert new_car.model == "Sandero"
+    assert new_car.color == "red"
     assert str(new_car) == "CJ 01 ABC -> Dacia Sandero, red"
 
     # repaint it
-    new_car.set_color("blue")
-    assert new_car.get_color() == "blue"
+    # new_car.set_color("blue")
+    new_car.color = "blue"
+    assert new_car.color == "blue"
     assert str(new_car) == "CJ 01 ABC -> Dacia Sandero, blue"
 
     # change license plates
-    new_car.set_id("CJ 99 XYZ")
+    new_car.car_id = "CJ 99 XYZ"
     assert str(new_car) == "CJ 99 XYZ -> Dacia Sandero, blue"
+
+
+if __name__ == "__main__":
+    test_car()
