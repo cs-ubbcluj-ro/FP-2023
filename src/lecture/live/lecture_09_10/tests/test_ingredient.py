ac0de34605089c52733a75962545d6cce11d6a0e
Arthur Molnar <arthur486@gmail.com>
2022-12-15 10:02:30 +0200

    Live coding week 11 lecture (1/2, wednesday)

---
+++ src/lecture/live/lecture_09_10/tests/test_ingredient.py
@@ -0,0 +1,25 @@
+import unittest
+
+from lecture.live.lecture_09_10.domain.ingredient import Ingredient
+
+
+class Test_Ingredient(unittest.TestCase):
+    def test_ingredient1(self):
+        ingr = Ingredient(1000, "White Flour")
+        self.assertEqual(ingr.id, 1000)
+        self.assertEqual(ingr.name, "White Flour")
+
+    def test_ingredient2(self):
+        ingr = Ingredient(1000, "White Flour")
+        self.assertEqual(ingr.id, 42)
+        self.assertEqual(ingr.name, "White Flour")
+
+    def test_ingredient3(self):
+        ingr = Ingredient(1000, "White Flour")
+        self.assertEqual(ingr.id, 1000)
+        self.assertEqual(ingr.name, "White Flour")
+
+    def test_ingredient4(self):
+        ingr = Ingredient(1000, "White Flour")
+        self.assertEqual(ingr.id, 1000)
+        self.assertEqual(ingr.name, "White Flour")
