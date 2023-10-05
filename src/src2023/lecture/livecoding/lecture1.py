8c6dacc0388fb0ff520e54e057894ca078c6bd99
Arthur Molnar <arthur486@gmail.com>
2023-10-05 08:49:51 +0300

    first program menu working

--- src/src2023/lecture/livecoding/lecture1.py
+++ src/src2023/lecture/livecoding/lecture1.py
@@ -1,1 +1,28 @@
-print("hello world")
+# print("hello world")
+
+"""
+Display a menu
+    1. Enter numbers
+    2. Exit
+    
+after entering numbers, print out the prime ones
+"""
+
+while True:
+    print("1. Enter numbers")
+    print("2. Exit")
+    option = input(">")
+    if option == "1":
+        numbers = input("enter numbers: ")
+        print(numbers)
+    elif option =="2":
+        print("bye")
+        break
+    else:
+        print("error")
+
+
+
+
+
+
