58928eaac0f29b28c8c8541417c57f993e375ff2
anamariabriciu <anamaria.briciu@ubbcluj.ro>
2023-10-06 13:10:18 +0300

    Implemented first version of calculator

--- src/src2023/seminar/group917/seminar1.py
+++ src/src2023/seminar/group917/seminar1.py
@@ -1,2 +1,24 @@
-# This is the code for Seminar 1
-print("Testing!")
+print("This is a calculator app. Available operations: +, -, *, /. exit to stop.")
+userlist = []
+while True:
+    cmd = input(">>>")
+    print(cmd)
+
+    if cmd.lower().strip() == 'exit':
+        break
+    else:
+
+        userlist = cmd.split()
+        a = int(userlist[0])
+        b = int(userlist[2])
+        if userlist[1] == '+':
+            print(a+b)
+        elif userlist[1] == '-':
+            print(a-b)
+        elif userlist[1] == '*':
+            print(a*b)
+        elif userlist[1] == '/':
+            print(a/b)
+
+# if 'exit' in cmd.lower():
+#     break
