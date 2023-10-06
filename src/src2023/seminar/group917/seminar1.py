46cd16eecd73123d7c81a3fe6f03364bdd7af894
anamariabriciu <anamaria.briciu@ubbcluj.ro>
2023-10-06 19:16:06 +0300

    Moved computing code to function

--- src/src2023/seminar/group917/seminar1.py
+++ src/src2023/seminar/group917/seminar1.py
@@ -1,26 +1,40 @@
+def compute(a: int, b: int, symbol: str) -> float:
+    if symbol == '+':
+        return a + b
+    elif symbol == '-':
+        return a - b
+    elif symbol == '*':
+        return a * b
+    elif symbol == '/':
+        return a / b
+
+
+# TO DO (modifications): add validation function
+# renaming (optional)
+#
 print("This is a calculator app. Available operations: +, -, *, /. exit to stop.")
-userlist = []
+operations = '+-/*'
 while True:
     cmd = input(">>>")
-    print(cmd)
+    op_count = 0
+    # for index in range(len(operations)):
+    for op in operations:
+        op_count += cmd.count(op)
 
+    if op_count != 1:
+        print("Invalid command. Please try again.")
+        continue
     if cmd.lower().strip() == 'exit':
         break
     else:
-
         userlist = cmd.split()
+        if len(userlist) != 3:
+            print("Please enter a valid command.")
+            continue
+
         a = int(userlist[0])
         b = int(userlist[2])
-        if userlist[1] == '+':
-            print(a+b)
-        elif userlist[1] == '-':
-            print(a-b)
-        elif userlist[1] == '*':
-            print(a*b)
-        elif userlist[1] == '/':
-            print(a/b)
-
-        print("fsdf")
-
+        result = compute(a, b, userlist[1])
+        print('Result is:', result)
 # if 'exit' in cmd.lower():
 #     break
