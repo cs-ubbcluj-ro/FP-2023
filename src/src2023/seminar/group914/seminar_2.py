ad1a0d832ae3bbc77dc92cb0c43695b4f53a270e
Arthur Molnar <arthur486@gmail.com>
2023-10-09 10:23:14 +0300

    defined problem requirements

--- src/src2023/seminar/group914/seminar_2.py
+++ src/src2023/seminar/group914/seminar_2.py
@@ -4,5 +4,12 @@ We want to get some practice with:
     2. Writing a menu-driven console application
     3. Generating some numbers :)
     4. Representing program entities
+
+Let's implement something - Manage a list of rational numbers
+    Functionalities:
+        1. Add a new rational number from console
+        2. Adding some random numbers programmatically
+        3. Sort the list of numbers
+        4. Exit the program
 """
 
