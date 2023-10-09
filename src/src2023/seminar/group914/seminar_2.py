9c1b380b9bc3298a8d6daf58036de9d14384484c
Arthur Molnar <arthur486@gmail.com>
2023-10-09 10:57:10 +0300

    pythons lists and dict intro

--- src/src2023/seminar/group914/seminar_2.py
+++ src/src2023/seminar/group914/seminar_2.py
@@ -13,3 +13,43 @@ Let's implement something - Manage a list of rational numbers
         4. Exit the program
 """
 
+
+# Functions for rational numbers
+# create a number, compare 2 numbers
+
+# Data type recap: int, float, boolean, str, tuple, list, dict
+# we keep the numbers in a Python list
+# we represent each number as a dict
+
+def create_q(num: int, den: int) -> dict:
+    """
+    Create a new rational number
+    :param num: numerator value
+    :param den: denominator value
+    :return: the newly created number
+    """
+    return {"num": num, "den": den}
+
+
+def to_str(q: dict) -> str:
+    """
+    Return the number's string representation
+    :param q: The rational number
+    :return: str representation
+    """
+    return str(q["num"]) + "/" + str(q["den"])
+
+
+print(create_q(1, 4))
+print(to_str(create_q(1, 4)))
+
+# Program functionalities
+
+data = [create_q(1, 2), create_q(2, 3), create_q(4, 5)]
+print(data)
+
+for i in range(0, len(data)):
+    print(to_str(data[i]))
+
+for q in data:
+    print(to_str(q))
