63fe173b280d338c898eb5f9ed3cc5651fdcce0d
Arthur Molnar <arthur486@gmail.com>
2023-10-09 14:40:33 +0300

    911 - added problem statement

--- src/src2023/seminar/group911/seminar_2.py
+++ src/src2023/seminar/group911/seminar_2.py
@@ -1,1 +1,11 @@
-print("Hello group 911!")
+# print("Hello group 911!")
+
+"""
+Manage a list of circles
+    1. Add a circle from console
+    2. Add a random circle
+    3. Display all circles
+    4. Sort circles by radius
+    0. Exit the program
+"""
+print("Hello circle!")
