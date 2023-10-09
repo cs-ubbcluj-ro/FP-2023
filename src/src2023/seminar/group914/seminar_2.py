8331419ec0ef5dbd1655548f7f9dad845c952ad5
Arthur Molnar <arthur486@gmail.com>
2023-10-09 10:18:52 +0300

    what do we learn today?

--- src/src2023/seminar/group914/seminar_2.py
+++ src/src2023/seminar/group914/seminar_2.py
@@ -1,1 +1,8 @@
-print("Hello group 914!")
+"""
+We want to get some practice with:
+    1. Python 3
+    2. Writing a menu-driven console application
+    3. Generating some numbers :)
+    4. Representing program entities
+"""
+
