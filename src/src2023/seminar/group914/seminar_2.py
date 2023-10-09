823e7459beaf7b6ee5324b814f7fa4d3f75e99c8
Arthur Molnar <arthur486@gmail.com>
2023-10-09 11:51:51 +0300

    finished seminar with 914

--- src/src2023/seminar/group914/seminar_2.py
+++ src/src2023/seminar/group914/seminar_2.py
@@ -12,6 +12,8 @@ Let's implement something - Manage a list of rational numbers
         3. Sort the list of numbers
         4. Exit the program
 """
+from random import randint
+from math import gcd
 
 
 # Functions for rational numbers
@@ -28,7 +30,9 @@ def create_q(num: int, den: int) -> dict:
     :param den: denominator value
     :return: the newly created number
     """
-    return {"num": num, "den": den}
+    # TODO Denominator cannot be 0
+    g = gcd(num, den)
+    return {"num": num // g, "den": den // g}  # // - integer division
 
 
 def to_str(q: dict) -> str:
@@ -37,19 +41,59 @@ def to_str(q: dict) -> str:
     :param q: The rational number
     :return: str representation
     """
+    if q["den"] == 1:
+        return str(q["num"])
     return str(q["num"]) + "/" + str(q["den"])
 
 
-print(create_q(1, 4))
-print(to_str(create_q(1, 4)))
+# print(create_q(1, 4))
+# print(to_str(create_q(1, 4)))
 
 # Program functionalities
 
 data = [create_q(1, 2), create_q(2, 3), create_q(4, 5)]
-print(data)
+while True:
+    print("")
+    print("1. Add a number from console")
+    print("2. Add random number")
+    print("3. Display numbers")
+    print("0. Exit")
 
-for i in range(0, len(data)):
-    print(to_str(data[i]))
+    option = input(">")
 
-for q in data:
-    print(to_str(q))
+    if option == "1":
+        number_str = input("number: ")
+        tokens = number_str.split("/")
+        # TODO Program crash at invalid input
+        numerator = int(tokens[0])  # Python str
+        denominator = int(tokens[1])
+
+        if denominator == 0:
+            print("Denominator cannot be 0")
+        else:
+            q = create_q(numerator, denominator)
+            data.append(q)
+    elif option == "2":
+        numerator = randint(-10, 10)
+        denominator = randint(1, 10)  # avoid generating 0 for denominator
+        q = create_q(numerator, denominator)
+        data.append(q)
+
+    elif option == "3":
+        print("The list is")
+        for q in data:
+            print(to_str(q), end=' ')  # end is a named python parameter
+
+    elif option == "0":
+        print("bye!")
+        break
+    else:
+        print("Bad command or file name!")
+
+#
+# data = [create_q(1, 2), create_q(2, 3), create_q(4, 5)]
+# print(data)
+#
+# for i in range(0, len(data)):
+#     print(to_str(data[i]))
+#
