5645258c465dae69c15e6631509120b960aae292
Arthur Molnar <arthur486@gmail.com>
2023-10-09 14:51:20 +0300

    911 - added circle representation

--- src/src2023/seminar/group911/seminar_2.py
+++ src/src2023/seminar/group911/seminar_2.py
@@ -9,3 +9,26 @@ Manage a list of circles
     0. Exit the program
 """
 print("Hello circle!")
+
+"""
+    How do we represent a circle?
+        circle center (x,y) and radius (r) -- all integers, r > 0
+        
+"""
+c1 = {"x": 1, "y": 2, "r": 3}
+c2 = {"x": 3, "y": 4, "r": 5}
+print([c1, c2])
+
+# Decouple the circle's representation from the program
+# (the program should not know that the circle is a dict)
+# Circle functions
+
+
+# Program functionalities
+
+
+# circle center (1,2), radius 3 => {"x":1,"y":2,"r":3}
+# d = {1: "Alice", 2: "Bob", 3: [1, 2, 3]}
+# # d[3] = "Xavier"
+# print(d, type(d))
+# print(d.keys(), d.values())
