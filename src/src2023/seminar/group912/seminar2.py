ef189ddca6918947c4bcf385c010c30490304ff4
Arthur Molnar <arthur486@gmail.com>
2023-10-10 18:14:20 +0300

    group 912 = problem statement defined

---
+++ src/src2023/seminar/group912/seminar2.py
@@ -0,0 +1,15 @@
+"""
+What we want to learn today:
+    - menu-driven application
+    - Python lists and dictionaries (a little bit)
+    - manage a list of program entities (circles)
+    - how to represent program entities (without classes and objects)
+
+Problem statement
+    A program that manages a list of circles
+    A circle has a center (x,y) and a radius (r), r > 0, all ints
+        - Add a circle from the console
+        - Generate random circles
+        - Sort the list by circle radius
+        - Exit the program :)
+"""
