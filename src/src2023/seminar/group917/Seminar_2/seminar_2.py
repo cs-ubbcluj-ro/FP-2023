f7fa05c4833a21e003622543074f1d593c68784b
anamariabriciu <anamaria.briciu@ubbcluj.ro>
2023-10-13 19:38:45 +0300

    Added comments and indications
    Added some comments regarding things we talked about at the seminar, but only wrote on the board.

--- src/src2023/seminar/group917/Seminar_2/seminar_2.py
+++ src/src2023/seminar/group917/Seminar_2/seminar_2.py
@@ -5,34 +5,38 @@ After the seminar, you will be able to:
     - manage program entities
         - representation
         - manipulation
-    - divide the program into circle-functions and program functionalities
-
+"""
+"""
 Problem statement
     A program that manages a list of TO-DO tasks.
     A TO-DO task has a description (description, str) and day of deadline
         (deadline_day, int, 1<deadline_day<365)
 
     The program must allow the following actions through a menu:
-        - Add a TO-DO task from the console
-        - Generate random tasks (descriptions are chosen from predefined set)
-        - Sort the list of tasks by deadline
+        - [Seminar] Add a TO-DO task from the console
+        - [Seminar] Generate random tasks (descriptions are chosen from predefined set)
+        - [A] Sort the list of tasks by deadline
         - [+] Print the list of tasks by day
         - [+] Count the number of tasks in a given interval of days (e.g. 3-100)
         - Exit the program
 """
+
 import random
 
-DESCRIPTIONS = ['BookSkydivingAdventure', 'HostMovieMarathon', 'TravelToUnchartedIsland',
-                'LearnJuggling', 'StartVegetableGarden', 'BuildTreehouse', 'VisitAlpacaFarm',
-                'MasterArtOfOrigami', 'StartBookClub', 'WriteSong']
+PREDEFINED_DESCRIPTIONS = ['BookSkydivingAdventure', 'HostMovieMarathon', 'TravelToUnchartedIsland',
+                           'LearnJuggling', 'StartVegetableGarden', 'BuildTreehouse', 'VisitAlpacaFarm',
+                           'MasterArtOfOrigami', 'StartBookClub', 'WriteSong']
 
 
+# functions that manage my program entity, a task
+# other parts of the program should only use these functions
+# and not access the representation directly
 def create_task(description, deadline):
     """
     Create task with given description
     :param description: description of the task
     :param deadline: day of deadline for task
-    :return:
+    :return: newly created task
     """
     return {"description": description, "deadline": deadline}
 
@@ -46,20 +50,49 @@ def get_deadline(task):
 
 
 # Uijfalusi Abel
+# Q: is read_task a good name for the function? does it do reading/only reading?
 def read_task():
     description = input("Please provide a task description:\n")
-    deadline = int(input("Please provide a deadline"))
+    deadline = int(input("Please provide a deadline:"))
     task = create_task(description, deadline)
     return task
 
 
 def print_task_list(task_list):
+    # Q: In what other ways could we write the print code? Hint: check other seminar problems+solutions
+    print('This is the current list of tasks:')
     for task in task_list:
         print('Description:', get_description(task), '| Deadline day:', get_deadline(task))
 
+    #different ways to go through the list
+    #for index in range(len(task_list)):
+    #   task = task_list[index]
+    #   print('['+str(index)+']', 'Description: ', get_description(task), 'Deadline day:', get_deadline(task))
+    #for task_index, task in enumerate(task_list):
+    #   print('['+str(task_index)+']', 'Description: ', get_description(task), 'Deadline day:', get_deadline(task))
+
 
 def add_to_list(task_list, task):
+    """
+    Adds the given task to the list of tasks
+    :return: modifies the task_list by adding the given task at the end
+    """
     task_list.append(task)
+
+
+# check out https://pythontutor.com/render.html#mode=edit
+# for a cool visualization of code (especially useful to visualize
+# what happens in code like the above, and in functions like the other
+# one we discussed, e.g.
+# def modify_list_by_reassignment(tl):
+#   tl = [new_task_1, new_task_2,...]
+# which, in calling modify_list_by_reassignment(task_list) wouldn't actually modify task_list
+# you could also try:
+# a=[1,2,3]
+# b=a
+# b.append(4)
+# print(a)
+# to see what happens :)
 
 
 # data representation
@@ -86,20 +119,21 @@ while True:
     else:
         print("Invalid command. Enter number (between 0-4).")
         continue
-
+    if option == 0:
+        print_task_list(task_list)
     # also see match-case syntax
     if option == 1:
         task = read_task()
         add_to_list(task_list, task)
-        print(task_list)
     elif option == 2:
         # generate task
         day = random.randint(1, 365)
-        description_index = random.randint(0, len(DESCRIPTIONS) - 1)
-        description = DESCRIPTIONS[description_index]
+        # we could have also used: random.choice
+        description_index = random.randint(0, len(PREDEFINED_DESCRIPTIONS) - 1)
+        description = PREDEFINED_DESCRIPTIONS[description_index]
         task = create_task(description, day)
         add_to_list(task_list, task)
-        print_task_list(task_list)
+
     elif option == 3:
         # sort tasks
         pass
@@ -107,6 +141,8 @@ while True:
         break
     else:
         print('Invalid command.')
+
+
 
 # Working with dictionaries
 # let's define a dict that contains information about persons
@@ -128,3 +164,7 @@ while True:
 #     print('CNP:', person_id)
 #     for person_attribute, attribute_value in person_information.items():
 #         print(person_attribute, attribute_value)
+#for person_id in persons:
+#   ...
+#persons.keys() -> all the keys in my dictionary
+#persons.values() -> all values in my dictionary
