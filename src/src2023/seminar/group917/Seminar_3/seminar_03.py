75939aa61c3568780a93e7d81ad3a56b76f50812
anamaria-briciu <anamaria.briciu15@gmail.com>
2023-10-20 17:52:51 +0300

    Uploaded code for seminar 3
    A separate scanned file will be uploaded in the following days with the computations we wrote on the board.

--- src/src2023/seminar/group917/Seminar_3/seminar_03.py
+++ src/src2023/seminar/group917/Seminar_3/seminar_03.py
@@ -3,31 +3,69 @@
     source: https://complex-systems-ai.com/en/algorithmic/corrected-exercises-time-complexity/
 """
 
+"""
+Complexity analysis
 
+Notations:
+    - n = size of the input (number of elements that have to be processed - but we can also have n, m, ... if multiple inputs)
+    - A = the algorithm
+    - T(n) = number of steps (operations) the algorithm does
+    
+    - O(n) - upper bound - my algorithm cannot go worse than this
+    - Omega(n) - low bound - my algorithm cannot go better than this
+    - Theta(n) - both upper and lower bound - my algorithm always has the same complexity (BC=AC=WC)
+    
+Cases
+    - BC(A): case in which we do least number of steps
+    - AC(A): formula
+    - WC(A): case in which we do most steps
+    
+Complexity in terms of:
+    - time: number of operations taken to solve the problem
+    - space
+"""
+
+
+# Terebent Roxana
+# T(n) = 10*n,
+# O(n) - upper bound
+# Theta(n) - it always performs 10*n steps
+#           (no cases in which it does less/more)
 def f_1(n: int):
-    for i in range(10):
-        for j in range(n):
+    for i in range(10):  # i=0..9
+        for j in range(n):  # j=0..n
             print("Hello World")
 
 
+# Voda Ioan
+# T(n) = 10*n, O(n)/Theta(n)
 def f_2(n: int):
-    for i in range(n, n + 10):
+    for i in range(n, n + 10):  # i=n+0, n+1 .. n+9
+        # it goes in constant number of steps (10 steps)
         for j in range(n):
+            # it depends to n - j = 0,1..n-1
             print("Hello World")
 
 
+# Schiop Bogdan
+# T(n) = n(n-1)/2 = O(n^2)
+# explicit computations (will be) available (shortly) in separate file
 def f_3(n: int):
-    for i in range(1, n):  # 1 .. (n-1)
-        for j in range(i, n):  #
+    for i in range(1, n):  # i = 1 .. (n-1)
+        for j in range(i, n):  # j = i, i+1..n-1
             print("Hello World")
 
 
+# Serban Dragos
+# O(n^2)
 def f_4(n: int):
     for i in range(n):
         for j in range(2 * i + 1):
             print("Hello World")
 
 
+# Tiut Cristian
+# T(n) = (n^2-1)*n^2 -> O(n^4)
 def f_5(n: int):
     for i in range(n ** 2):
         for j in range(i):
@@ -46,11 +84,12 @@ for i in range(13):
     print(i, f_run(i))
 
 
+# O(n*log2(n))
 def f_6(n: int):
-    for i in range(n):  # n iterations
+    for i in range(n):  #0..n
         t = 1
-        while t < n:  # log2(n) -- log base 2 of n
-            print("Hello World")
+        while t < n:  # how many times can we multiply by 2 until we get to n?
+            print("Hello World") #A: as many times as n can be divided by 2 -> log2(n)
             t *= 2
 
 
@@ -59,6 +98,8 @@ def f_6(n: int):
 """
 
 
+# Szarics Iulia
+# O(n+m)
 def f_7(n, m: int):
     for i in range(0, n):  # n iterations
         print("Hello World")
@@ -66,6 +107,8 @@ def f_7(n, m: int):
         print("Hello World")
 
 
+# Tiganasu Alexandru
+# T(n) = 2*n ->  O(n)
 def f_8(n, m: int):
     for i in range(0, n):  # n iterations
         print("Hello World")
@@ -73,6 +116,8 @@ def f_8(n, m: int):
         print("Hello World")
 
 
+# Ungur Andreea
+# T(n) =n * (n+n) = 2n^2 -> O(n^2)
 def f_9(n: int):
     for i in range(n):
         for j in range(n):
@@ -80,7 +125,7 @@ def f_9(n: int):
         for k in range(n):
             print("Hello World")
 
-
+#TO-DO
 def f_10(n: int):
     for i in range(n):
         for j in range(n, i, -1):
@@ -92,6 +137,8 @@ Analyze the time and space complexity
 """
 
 
+# Time: O(n*logn) (base of log = 3)
+# Extra space complexity: O(1)
 def f_11(data: list):
     data_sum = 0
     for el in data:
@@ -102,6 +149,8 @@ def f_11(data: list):
     return data_sum
 
 
+#Time complexity: O(n)
+#Space complexity: ? (!!slicing)
 def f_12(data: list):
     if len(data) == 0:
         return 0
