1625c23d845608ac86ffc48f4a9d61460bba91a2
anamaria-briciu <anamaria.briciu15@gmail.com>
2023-11-02 22:40:15 +0200

    Added implementations for max_subarray_sum
    To discuss at seminar

---
+++ src/src2023/seminar/group917/Seminar_5/max_subarray_sum.py
@@ -0,0 +1,109 @@
+import math
+import random
+
+
+def generate_list(n: int) -> list:
+    """
+    Generate a list of length n filled with pseudo random integers
+    :param n: the number of integers to put in the list
+    :type n: int
+    :return: list of numbers
+    :rtype: list
+    """
+    return [random.randint(-n, n) for i in range(n)]
+
+
+"""
+Complexity? 
+T(n) = ...
+"""
+
+
+# Naive implementation: generate all subsets and compute sum for each
+def max_sum_naive(array: list) -> (int, int, int):
+    """
+    Compute maximum subarray sum and start&end indices for corresponding subarray
+    :param array: list in which we search the subset
+    :type array: list
+    :return: maximum sum, and the start and end indices for the subarray, in this order
+    :rtype: tuple - (int, int, int)
+    """
+    max_subarray_sum = -math.inf
+    subarray_end = -1
+    subarray_begin = -1
+
+    for i in range(len(array)):
+        for j in range(i + 1, len(array) + 1):
+            current_subset = array[i:j]
+            current_sum = sum(current_subset)
+
+            print('Current subset is:', current_subset, 'with sum', current_sum)
+
+            if current_sum > max_subarray_sum:
+                max_subarray_sum = current_sum
+                subarray_end = j
+                subarray_begin = i
+
+    return max_subarray_sum, subarray_begin, subarray_end
+
+
+"""
+Complexity?
+T(n) = ...
+"""
+
+
+# Divide and conquer implementation: divide in halves + take 'crossing' arrays
+def max_sum_dc(array: list, left: int, right: int):
+    if left == right:
+        return array[left]
+
+    middle = (left + right) // 2
+    left_sum = max_sum_dc(array, left, middle)
+    right_sum = max_sum_dc(array, middle + 1, right)
+
+    left_sum_crossing = 0
+    max_left_crossing = -math.inf
+    for i in range(middle, left - 1, -1):
+        left_sum_crossing += array[i]
+        if left_sum_crossing > max_left_crossing:
+            max_left_crossing = left_sum_crossing
+
+    right_sum_crossing = 0
+    max_right_crossing = -math.inf
+    for i in range(middle + 1, right + 1):
+        right_sum_crossing += array[i]
+        if right_sum_crossing > max_right_crossing:
+            max_right_crossing = right_sum_crossing
+
+    max_crossing_sum = max_left_crossing + max_right_crossing
+    return max(left_sum, right_sum, max_crossing_sum)
+
+
+"""
+Complexity?
+T(n) = ...
+"""
+
+
+def max_sum_dp(arr):
+    max_sum = -math.inf
+    crt_sum = arr[0]
+    for i in range(1, len(arr)):
+        crt_sum = max(arr[i], crt_sum + arr[i])
+        max_sum = max(crt_sum, max_sum)
+
+    return max_sum
+
+
+lst = generate_list(10)
+print('List is:', lst)
+max_sum, max_sum_begin, max_sum_end = max_sum_naive(lst)
+print('Subarray with max sum is', lst[max_sum_begin:max_sum_end], 'with sum', max_sum)
+res_max_sum_dc = max_sum_dc(lst, 0, len(lst) - 1)
+print('Max subarray sum with D&C is', res_max_sum_dc)
+
+res_max_sum_dp = max_sum_dp(lst)
+print('Max subarray sum with DP is', res_max_sum_dp)
+
+assert (max_sum == res_max_sum_dc == res_max_sum_dp)
