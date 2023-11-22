1c6859fc423a71d595f6091eb64b57961e06e4ac
anamaria-briciu <anamaria.briciu15@gmail.com>
2023-11-22 19:22:59 +0200

    Added checks for board status

--- src/src2023/seminar/group917/Seminar_7/board.py
+++ src/src2023/seminar/group917/Seminar_7/board.py
@@ -1,5 +1,7 @@
-
 # Voda Ioan
+import numpy as np
+
+
 def generate_board():
     """
     Generate empty board (empty: with values -1)
@@ -37,6 +39,87 @@ def update_position(board, row, column, symbol):
     board[row - 1][column - 1] = symbol
 
 
+def get_symbol_at_postion(board, row, column):
+    """
+    Get symbol at position row, column on the board
+    :param board: board we are checking
+    :type board: list
+    :param row: row number
+    :type row: int
+    :param column: column number
+    :type column: int
+    :return: symbol at position row, column on board
+    :rtype:
+    :raises: ValueError if row, column are invalid
+    """
+    if not 0 < row < 4:
+        raise ValueError("Invalid row.")
+
+    if not 0 < column < 4:
+        raise ValueError("Invalid column.")
+
+    return board[row - 1][column - 1]
+
+
+def get_board_status(board) -> int:
+    """
+    Checks if someone won the game
+    :param board: the board we are checking
+    :type board: list
+    :return: code corresponding to status of board
+            1 - won by human
+            2 - won by computer
+            0 - draw
+            3 - game is still being played
+    :rtype: int
+    """
+    # Q: is implementation efficient?
+    # Q: will it work if we change the board (e.g. the size)?
+
+    winning_x = ['X', 'X', 'X']
+    winning_o = ['O', 'O', 'O']
+
+    for i in range(3):
+        row = board[i]
+        if row == winning_x:
+            return 1
+        if row == winning_o:
+            return 2
+
+        column = [row[i] for row in board]
+        if column == winning_x:
+            return 1
+        if column == winning_o:
+            return 2
+
+    main_diagonal = [board[0][0], board[1][1], board[2][2]]
+    secondary_diagonal = [board[0][2], board[1][1], board[2][0]]
+    if main_diagonal == winning_x or secondary_diagonal == winning_x:
+        return 1
+    if main_diagonal == winning_o or secondary_diagonal == winning_o:
+        return 2
+
+    if is_full(board):
+        return 0
+    else:
+        return 3
+
+
+def is_full(board):
+    """
+    Checks if the given board is full
+    :param board: the board we are checking
+    :type board: list
+    :return: True if board is full, False otherwise
+    :rtype: bool
+    """
+    for row_index in range(3):
+        for col_index in range(3):
+            if board[row_index][col_index] == -1:
+                return False
+    return True
+
+
 def to_str(board):
     """
     Returns a string of the board for nice printing
@@ -46,8 +129,78 @@ def to_str(board):
         # .join(list/tuple/...) -> joins elements of the list/tuple/... IF they are all string, else raises Error
         # Initially: [str(elem) for elem in row]: create the list (see: parantheses []) which contains the string
         # representation (str(elem)) for each element in the row list ("for elem in row")
-        #Current version: if elem!=-1 means X or O -> take it as is (elem if elem!=-1 else put a - instead of
+        # Current version: if elem!=-1 means X or O -> take it as is (elem if elem!=-1 else put a - instead of
         # -1 (better visibility of empty positions) (else "-") where we go over each elem in list row (for elem in row)
         board_str += ' | '.join([elem if elem != -1 else "-" for elem in row])
         board_str += '\n'
     return board_str
+
+
+def test_update_postion():
+    board = generate_board()
+    update_position(board, 1, 1, 'X')
+    assert (get_symbol_at_postion(board, 1, 1) == 'X')
+
+    try:
+        update_position(board, 10, 1, 'X')
+        assert False
+    except ValueError:
+        assert True
+
+    try:
+        update_position(board, 1, 100, 'X')
+        assert False
+    except ValueError:
+        assert True
+
+    try:
+        update_position(board, 1, 1, 'X')
+        assert False
+    except ValueError:
+        assert True
+
+
+def test_check_status():
+    board = generate_board()
+    update_position(board, 1, 1, 'X')
+    update_position(board, 1, 3, 'X')
+    update_position(board, 2, 3, 'O')
+    assert (get_board_status(board) == 3)
+    update_position(board, 3, 1, 'O')
+    update_position(board, 3, 2, 'O')
+    update_position(board, 3, 3, 'X')
+    assert (get_board_status(board) == 3)
+    # board is now:
+    # X | - | X
+    # - | - | O
+    # O | O | X
+
+    update_position(board, 1, 2, 'X')
+    assert (get_board_status(board) == 1)
+
+    board2 = generate_board()
+    update_position(board2, 1, 1, 'X')
+    update_position(board2, 3, 1, 'O')
+    update_position(board2, 3, 2, 'O')
+    update_position(board2, 2, 2, 'O')
+    update_position(board2, 1, 2, 'O')
+    # X | O | -
+    # - | O | -
+    # O | O | -
+    assert (get_board_status(board2) == 2)
+
+    board3 = generate_board()
+    update_position(board3, 1, 1, 'X')
+    update_position(board3, 1, 3, 'X')
+    update_position(board3, 2, 3, 'O')
+
+    update_position(board3, 1, 2, 'O')
+    update_position(board3, 2, 1, 'X')
+    update_position(board3, 2, 2, 'O')
+    update_position(board3, 3, 1, 'O')
+    update_position(board3, 3, 2, 'X')
+    update_position(board3, 3, 3, 'X')
+    # X | O | X
+    # X | O | O
+    # O | X | X
+    assert (get_board_status(board3) == 0)
--- src/src2023/seminar/group917/Seminar_7/main.py
+++ src/src2023/seminar/group917/Seminar_7/main.py
@@ -1,6 +1,8 @@
 import game
-from board import generate_board, to_str
+from board import generate_board, to_str, get_board_status
 from ui import *
+
+
 
 
 def start():
@@ -15,7 +17,6 @@ def start():
 
             user_input = input(">>>").strip()
             command, params = user_input.split(" ", maxsplit=1)
-
             if command == 'play':
                 try:
                     x, y = check_play_cmd_params(params)
@@ -23,11 +24,27 @@ def start():
                     print("User wants to put an X on row", x, "column", y)
                 except ValueError as v:
                     print("Error occurred %s" % v)
+                    continue
             elif command == 'exit':
                 is_running = False
         else:
             row, column = game.make_computer_move(board_game, 'O')
             print("Computer moved to put an O on row", row, "column", column)
+
+        board_status = get_board_status(board_game)
+        if board_status == 1:
+            print(to_str(board_game))
+            print('Game was won by human player.')
+            return
+        elif board_status == 2:
+            print(to_str(board_game))
+            print('Game was won by computer player.')
+            return
+        elif board_status == 0:
+            print(to_str(board_game))
+            print('Game results in a draw.')
+            return
+
         user_turn = not user_turn
 
 
